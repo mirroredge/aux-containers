2a089a7b09ef3fc422883c5ac258312f193fb212
MikeS <mpschendel@gmail.com>
2020-02-26 11:35:39 -0500

    Add in locust, lockdown keycloak

--- keycloak/keycloak.sh
+++ keycloak/keycloak.sh
@@ -1,1 +1,1 @@
-docker run -p 8080:8080 -p 9990:9990 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name=keycloak jboss/keycloak
+docker run -p 8080:8080 -p 9990:9990 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin --name=keycloak jboss/keycloak:8.0.2
